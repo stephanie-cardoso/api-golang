7938e19880a0326139ed49af3350680b6956102b
Stephanie Cardoso <stephaug12@gmail.com>
2023-11-07 18:07:09 -0300

    Create route functions

--- router/router.go
+++ router/router.go
@@ -8,10 +8,41 @@ import (
 
 func Initialize() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/opening", getOpening)
+	router.POST("/opening", newOpening)
+	router.DELETE("/opening", deleteOpening)
+	router.PUT("/opening", updateOpening)
+	router.GET("/openings", getOpenings)
+
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func getOpening(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "get opening",
+	})
+}
+
+func newOpening(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "post opening",
+	})
+}
+
+func deleteOpening(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "delete opening",
+	})
+}
+
+func updateOpening(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "put opening",
+	})
+}
+
+func getOpenings(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "get openings",
+	})
 }
