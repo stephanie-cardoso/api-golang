6487cdb7d83b3f44ac99960754cebdcc7b5670c5
Stephanie Cardoso <stephaug12@gmail.com>
2023-12-03 15:48:46 -0300

    Add makefile to run swagger

--- router/router.go
+++ router/router.go
@@ -2,11 +2,15 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/stephanie-cardoso/api-golang/docs"
 	"github.com/stephanie-cardoso/api-golang/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Initialize() {
 	router := gin.Default()
+	docs.SwaggerInfo.BasePath = "/"
 	handler.InitializeHandler()
 
 	router.GET("/opening", handler.GetOpening)
@@ -14,6 +18,7 @@ func Initialize() {
 	router.DELETE("/opening", handler.DeleteOpening)
 	router.PUT("/opening", handler.UpdateOpening)
 	router.GET("/openings", handler.GetOpenings)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
